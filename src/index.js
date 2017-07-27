178ed5861ba5f5841155e2f7b8a87fdc5a2f1242
vviital <vviital@gmail.com>
2017-07-27 18:58:16 +0300

    Add upload to mongodb

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import { spawn } from 'child_process';
+import path from 'path';
+import through from 'through2';
+import split from 'split';
+import { MongoClient } from 'mongodb';
+
+const pathToScript = path.join(__dirname, '../lib/alis-web-index/bin');
+const pathToSnapshot = path.join(__dirname, '../snapshots/snapshot.txt');
+
+const url = 'url-to-database';
+
+const filterDocuments = (object, _, next) => {
+  if (object.record) return next(null, object.record);
+  return next();
+};
+
+const createWriter = collection => (object, _, next) =>
+  collection.insertOne(object, (err, __) => {
+    if (err) {
+      console.error(err);
+      return process.exit(1);
+    }
+
+    return next();
+  })
+;
+
+MongoClient.connect(url, (err, db) => {
+  if (err) process.exit(1);
+
+  console.log('connected to database');
+
+  const rawDataSet = db.collection('raw-data-set');
+
+  const child = spawn('babel-node', [pathToScript, '-s', pathToSnapshot]);
+
+  child.stdout
+    .pipe(split(JSON.parse))
+    .pipe(through.obj(filterDocuments))
+    .on('error', console.error)
+    .pipe(through.obj(createWriter(rawDataSet)));
+});
